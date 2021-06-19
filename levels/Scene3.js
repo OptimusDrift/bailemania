aa0562515b4a156e6e5d3d3101c91dd0c5303dec
v0lker01 <81654287+v0lker01@users.noreply.github.com>
2021-06-19 13:58:25 -0300

    agregado de flechas para jugador 2

--- levels/Scene3.js
+++ levels/Scene3.js
@@ -1,7 +1,10 @@
 var tiempoRestante;
-var keyX;
-var Score;
-var Puntos;
+var collidersPerfectoJ0;
+var collidersPerdidoJ0;
+var collidersCasiPerfectoJ0;
+var collidersPerfectoJ1;
+var collidersPerdidoJ1;
+var collidersCasiPerfectoJ1;
 class Scene3 extends Phaser.Scene {
   constructor() {
     super("Juegonivel2");
@@ -9,39 +12,66 @@ class Scene3 extends Phaser.Scene {
 
   preload() {}
   create() {
-    //Inicialización del Reloj
-    gameGlobalOptions.tiempoTotal = 0;
+        //Inicialización del Reloj
+        gameGlobalOptions.tiempoTotal = 0;
 
-    //Arreglos de flechas a agregar en el mapa
-    this.flechasGrupo = this.add.group();
+        //Arreglos de flechas a agregar en el mapa Jugador 0
+        this.flechasGrupoJ0 = this.add.group();
+    
+        //Flechas que estan activas en el mapa Jugador 0
+        this.flechaMPCJ0 = this.add.group();
+    
+        //Arreglos de flechas a agregar en el mapa Jugador 1
+        this.flechasGrupoJ1 = this.add.group();
+    
+        //Flechas que estan activas en el mapa Jugador 1
+        this.flechaMPCJ1 = this.add.group();
+    
+        //Colisiiones J0
+        collidersPerfectoJ0 = this.physics.add.staticGroup();
+        collidersPerfectoJ0.create(200, 470, "perfecto");
+        //collidersPerfecto.setVisible(false);
+    
+        collidersCasiPerfectoJ0 = this.physics.add.staticGroup();
+        collidersCasiPerfectoJ0.create(200, 520, "perfecto");
+        collidersCasiPerfectoJ0.create(200, 420, "perfecto");
+        //collidersCasiPerfecto.setVisible(false);
+    
+        collidersPerdidoJ0 = this.physics.add.staticGroup();
+        collidersPerdidoJ0.create(200, 590, "perfecto");
+    
+        //Colisiiones J1
+        collidersPerfectoJ1 = this.physics.add.staticGroup();
+        collidersPerfectoJ1.create(600, 470, "perfecto");
+        //collidersPerfecto.setVisible(false);
+    
+        collidersCasiPerfectoJ1 = this.physics.add.staticGroup();
+        collidersCasiPerfectoJ1.create(600, 520, "perfecto");
+        collidersCasiPerfectoJ1.create(600, 420, "perfecto");
+        //collidersCasiPerfecto.setVisible(false);
+    
+        collidersPerdidoJ1 = this.physics.add.staticGroup();
+        collidersPerdidoJ1.create(600, 590, "perfecto");
+    
+        //Pruebas borrar
+        t = 0;
 
-    //Flechas que estan activas en el mapa
-    this.flechaMPC = this.add.group();
+        //Jugador 1
+        this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
-    //Pruebas borrar
-    SpawnFlechas(this.flechaMPC, this.flechasGrupo, this.physics);
-    SpawnFlechas(this.flechaMPC, this.flechasGrupo, this.physics);
-    t = 0;
-    keyX = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
-
-    //Sistema de Puntos
-    Score = this.add.text(55, 545, "Puntos: 0", {fontFamily:"Impact, fantasy"});
-    Puntos = 0;
+        //Jugador 2
   }
   update(time, delta) {
     //Reloj del juego
     gameGlobalOptions.tiempoTotal += Reloj(delta);
     //Pruebas
     if (DesactivarReloj(gameGlobalOptions.tiempoTotal, t)) {
-      SpawnFlechas(this.flechaMPC, this.flechasGrupo, this.physics);
-      t = ActivarReloj(gameGlobalOptions.tiempoTotal, NumeroRandom(1, 0.2));
-    }
-    //console.log(Phaser.Input.Keyboard);
-    //a;
-    if (Phaser.Input.Keyboard.JustDown(keyX)) {
-      EliminarFlecha(this.flechasGrupo, "arriba");
-      Puntos += 50;
-      Score.setText("Puntos: " + Puntos);
+      SpawnFlechas(this.flechaMPCJ1, this.physics, 1);
+      SpawnFlechas(this.flechaMPCJ0, this.physics, 0);
+      t = ActivarReloj(gameGlobalOptions.tiempoTotal, NumeroRandom(0.5, 0.2));
     }
   }
 }
---
+++ scripts/ManejoDeNivel.js
@@ -0,0 +1,12 @@
+function AgregarPuntosCasiPerfecto () 
+{
+    Puntos += 50;    
+}
+function AgregarPuntosPerfecto () 
+{
+    Puntos += 100;    
+}
+function ReiniciarPuntos () 
+{
+    Puntos += 0;    
+}
